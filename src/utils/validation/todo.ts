0be1cb598e88903f4f2e8f19d9222ea2876cc85d
Sina Shafiee <alisinashafieezada@gmail.com>
2023-07-28 02:42:23 +0430

    fix - grammer

--- src/utils/validation/todo.ts
+++ src/utils/validation/todo.ts
@@ -4,11 +4,11 @@ import * as z from 'zod';
 export const addTodoSchema = z.object({
   title: z
     .string({ required_error: 'Title is Required' })
-    .min(4, { message: 'Title must be atleast 4 characters' })
+    .min(4, { message: 'Title must be at least 4 characters' })
     .max(15, { message: 'Title length must be under 15 char.' }),
   description: z
     .string({ required_error: 'Description is Required' })
-    .min(8, { message: 'Description must be atleast 8 characters' })
+    .min(8, { message: 'Description must be at least 8 characters' })
     .max(90, { message: 'Description length must be under 90 char.' }),
 });
 export type AddTodoValues = z.infer<typeof addTodoSchema>;
@@ -16,11 +16,11 @@ export type AddTodoValues = z.infer<typeof addTodoSchema>;
 export const updateTodoSchema = z.object({
   title: z
     .string({ required_error: 'Title is Required' })
-    .min(4, { message: 'Title must be atleast 4 characters' })
+    .min(4, { message: 'Title must be at least 4 characters' })
     .max(15, { message: 'Title length must be under 15 char.' }),
   description: z
     .string({ required_error: 'Description is Required' })
-    .min(8, { message: 'Description must be atleast 8 characters' })
+    .min(8, { message: 'Description must be at least 8 characters' })
     .max(90, { message: 'Description length must be under 90 char.' }),
   is_completed: z.boolean(),
 });
